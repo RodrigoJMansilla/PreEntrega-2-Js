d1eae69b6da6482f3f34aa77e59cfb1126e258d3
Rodrigo Mansilla <mansillarodrigoj@gmail.com>
2023-03-23 12:30:29 -0300

    commit inicial

---
+++ JS/main.js
@@ -0,0 +1,156 @@
+
+
+//funcion que recupera una fruta de gondola segun su id. Generalmente se usa enviando un codigo de los objetos instanciados de comprarFruta, para entender que fruta de la gondola es la que esta comprando el usuario en su pedido.
+function recuperaFruta(codigo){
+    encontrado = gondola.find((fruta)=>{return fruta.id === codigo})
+    return encontrado
+}
+
+
+
+// caso agregar producto
+function agregoCarrito(){
+    let codigo = parseInt(prompt(msjFrutas))
+
+    while(isNaN(codigo) || recuperaFruta(codigo)== undefined){
+            console.warn("Ha ingresado un valor erroneo. Por favor ingreselo nuevamente\n")
+            codigo = parseInt(prompt(msjFrutas))       
+    }
+
+    let cantidad = parseFloat(prompt(`Ingrese la cantidad de kg de ${recuperaFruta(codigo).nombre} deseada`))
+
+    while(cantidad < 0 || isNaN(cantidad)){
+        console.warn("Ha ingresado un valor de cantidad erroneo, por favor ingreselo nuevamente")
+        cantidad = parseFloat(prompt(`Ingrese la cantidad de kg de ${recuperaFruta(codigo).nombre} deseada`))
+    }
+
+    instancia = new comprarFrutas (codigo, cantidad)
+
+    let band1 = 0
+
+    for(fruta of pedidoFrutas){
+        if(fruta.codigo === instancia.codigo){
+            band1 = 1
+            if(confirm(`Su pedido ya incluye ${fruta.cantidadKg} Kg de ${recuperaFruta(fruta.codigo).nombre} ¿Desea agregar otros ${instancia.cantidadKg} Kg ?`)){
+                fruta.cantidadKg = fruta.cantidadKg + instancia.cantidadKg
+            }else{
+                console.warn("Su pedido no fue modificado.")
+            }
+        }
+    }
+
+    if(band1 == 0){
+        instancia.confirmarAgregado()
+    }
+    
+}
+
+
+/* ACTIVIDAD VER DE IMPLEMENTAR CON FOREACH */
+// funcion que calcula el total del costo del pedido
+function calcularTotal(){
+    let acum = 0
+    for(fruta of pedidoFrutas){
+        acum += parseFloat(fruta.consultarPrecio())
+    }
+    return acum
+}
+
+
+
+
+//caso ver pedido
+//funcion que muestra todo el pedido 
+function verPedido(){
+    console.log('Su pedido esta formado por:\n')
+    for(fruta of pedidoFrutas){
+        let estaFruta = gondola.find((i)=>{ return i.id === fruta.codigo })
+
+        console.log(`${fruta.cantidadKg}Kg de ${estaFruta.nombre} por $${fruta.consultarPrecio()}`)
+
+    }
+    acum = calcularTotal()
+    console.log(`El costo total de su pedido es de: $${acum}`)
+}
+
+
+//funcion que calcula cuotas
+function calculaCuotas(precio, i){
+    
+    if(i==3){
+        precio *= 1.2
+        return precio/i
+    }
+    else if(i==6){
+        precio *= 1.45
+        return precio/i
+    }
+}
+
+//caso efectuar la compra
+//funcion que corrobora la accion de finalizar y ofrece cuotas
+function finalizarCompra(){
+    alert(`El precio final de su carrito es de: $${calcularTotal()}`)
+
+    if(confirm("Desea pagar en cuotas?")){
+        iCuotas = parseInt(prompt(msjCuotas))
+        
+        while((iCuotas !== 3 && iCuotas !== 6) || isNaN(iCuotas)){
+            console.warn("El numero de cuotas debe ser 3 o 6, ingreselo nuevamente.\n")
+            iCuotas = parseInt(prompt("ingrese '3' o '6' de acuerdo a la cantidad de cuotas requeridas."))
+        }
+
+        alert("El pago se realizara en " + iCuotas + " cuotas de $" + calculaCuotas(calcularTotal(), iCuotas))
+    }
+    direccionCliente = prompt("Ingrese la dirección de envío:")
+    alert("Muchas Gracias por su compra! ya estamos preparando su pedido.")
+}
+
+// funcion resetear: inicializa las variables de nuevo a 0.
+function resetear(){
+    pedidoFrutas=[]
+}
+
+// funcion iniciar: genera el loop del menu
+function iniciar(){
+    resetear()
+    while(continuar){
+        menu()
+    }
+}
+
+//funcion menu
+function menu(){
+    choice = parseInt(prompt(msjMenu))
+    switch(choice){
+        case 1:
+            // caso agregar producto
+            agregoCarrito()
+            break
+        case 2:
+            // caso ver las frutas del carrito
+            verPedido()
+            break
+        case 3:
+            // caso ver costo total del carrito
+            if(confirm("¿Desea Finalizar su compra?")){
+                finalizarCompra()
+                actualizarStock(pedidoFrutas)
+                continuar = false
+            }
+            break
+        default:
+            break
+    }
+}
+
+// funcion que resta todo el stock vendido de "pedidoFrutas" del stock de gondola. Esta funcion como no interactua con el usuario, se puede corroborar viendo el array gondola, luego de finalizada la compra.
+function actualizarStock(arrayVenta){
+    let frutaGondola
+    arrayVenta.forEach((fruta)=>{
+        frutaGondola = recuperaFruta(fruta.codigo)
+        frutaGondola.stockKg = frutaGondola.stockKg - fruta.cantidadKg
+        gondola[frutaGondola.id-1].stockKg = frutaGondola.stockKg
+    })
+}
+
---
+++ JS/variables.js
@@ -0,0 +1,84 @@
+// constantes strings
+const msjMenu =     "Bienvenido a UvaScript! ¿En qué lo podemos ayudar?\n" +
+                    "Ingrese un número:\n" +
+                    "1) Agregar/Modificar frutas a su pedido\n" +
+                    "2) Ver las frutas en su pedido y el costo total del mismo\n" +
+                    "3) Efectuar la compra y terminar con su atención\n"
+
+const msjFrutas =   "Selecciona que fruta desea agregar al pedido:\n" +
+                    "Ingrese un número:\n" +
+                    "1) Banana\n" +
+                    "2) Manzana\n" +
+                    "3) Uvas\n" +
+                    "4) Sandía\n" +
+                    "5) Limón\n" +
+                    "6) Naranja\n"
+
+const msjCuotas =   "Ingrese la cantidad de cuotas en las que desea hacer el pago:\n" +
+                    "3) 20% Recargo\n" + 
+                    "6) 45% Recargo"
+
+// arrays
+//gondola es un array que contiene objetos frutas a vender. Estos objetos tienen nombre, precio/kg, stock en kg e id
+let gondola=[   {nombre: 'Banana', precio: 400, stockKg: 100, id: 1},
+                {nombre: 'manzana', precio: 800, stockKg: 150, id: 2},
+                {nombre: 'uva', precio: 2400, stockKg: 75, id: 3},
+                {nombre: 'sandia', precio: 150, stockKg: 200, id: 4},
+                {nombre: 'limon', precio: 300, stockKg: 120, id: 5},
+                {nombre: 'naranja', precio: 250, stockKg: 140, id: 6}    ]
+
+//pedidoFrutas este array vacio va a ir agregando objetos de la clase comprarFrutas
+let pedidoFrutas=[]
+
+//variables
+let iCuotas = 0
+let continuar = true
+let choice = 0
+
+
+/* 
+Clase ComprarFrutas
+Esta instancia objetos "Frutas" que van a ser compradas.
+Tiene dos propiedades:
+- codigo (se tiene que corresponder con la propiedad "id" de los objetos literales del array gondola)
+- cantidad (number que define la cantidad comprada de dicha fruta)
+
+tiene dos metodos:
+- consultarPrecio: recupera el precio/kg de la fruta en la gondola y devuelve el precio de la fruta en el pedido (cantidad * precio) Este metodo se utiliza en el siguiente metodo.
+- confirmarAgregado: Este muestra el costo de la cantidad de fruta a comprar y solicita una confirmacion para que la fruta sea agregada al pedido. Una vez confirmado, el metodo envia la instancia de la clase en cuestion (entera) al array que conforma el pedido "pedidoFrutas"
+
+*/
+class comprarFrutas {
+    constructor(codigo, cantidad){
+        this.codigo = codigo
+        this.cantidadKg = cantidad
+    }
+
+    consultarPrecio(){
+        
+        let coincidencia = gondola.find((fruta)=>{
+            return fruta.id === parseInt(this.codigo)
+        })
+
+        return parseFloat(coincidencia.precio * this.cantidadKg).toFixed(2)
+    }
+
+    confirmarAgregado(){
+
+        let coincidencia = gondola.find((fruta)=>{
+            return fruta.id === parseInt(this.codigo)
+        })
+
+        let costo = this.consultarPrecio()
+
+        let rta = confirm(`Usted desea ${this.cantidadKg}Kg de ${coincidencia.nombre} por un costo de ${costo}`)
+
+        if(rta){
+            pedidoFrutas.push(new comprarFrutas(this.codigo, this.cantidadKg)) // Esta es la forma que encontre para poder enviar una instancia de esta misma clase, con un metodo propio a un array que contenga el pedido.
+        }
+        else{
+            console.warn("El nuevo pedido fue cancelado.")
+        }
+    }
+}
+
